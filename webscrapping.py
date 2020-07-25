d9a4cee97516925391c16e53b89821a7c4de09d6
Dominik Capkovic <domcapkovic@gmail.com>
2020-07-25 23:21:59 +0200

    25.07.2020 version

--- webscrapping.py
+++ webscrapping.py
@@ -10,7 +10,7 @@ import csv
 csv_file = open("lidl.csv", "w")
 
 csv_writer = csv.writer(csv_file)
-csv_writer.writerow(["polozka", "cena"])
+csv_writer.writerow(["Položka", "Množství", "Cena"])
 
 source = requests.get("https://www.lidl.cz/aktualni-nabidka").text
 
@@ -20,21 +20,28 @@ nabidka = soup.find("div", class_ = "row")
 
 polozky = nabidka.find_all("div")
 
+
 for polozka in polozky:
-	# print(polozka.prettify())
+# 	print(polozka.prettify())
+
 	try:
 		polozka_nazev = polozka["data-name"]
 		polozka_cena  = polozka["data-price"]
+		polozka_mnozstvi = polozka.find("div", class_ = "product__text").p.text
 
-		csv_writer.writerow([polozka_nazev, polozka_cena])
 
-		print(polozka_nazev)
-		print(polozka_cena)
+		csv_writer.writerow([polozka_nazev, polozka_cena, polozka_mnozstvi])
 
-		print()
+		# print(polozka_nazev)
+		# print(polozka_cena)
+		# print(polozka_mnozstvi)
+
 	except Exception as e:
-		polozka_nazev = None
-		polozka_cena  = None
+		polozka_nazev    = None
+		polozka_cena     = None
+		polozka_mnozstvi = None
 
 
 csv_file.close()
+
+
